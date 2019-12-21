7d85040d6cd30b075eb27bc528a3a54574001563
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-12-21 12:07:35 -0500

    uptodate w/ TDAstats 0.4.0

--- R/ripser_func.R
+++ R/ripser_func.R
@@ -23,7 +23,7 @@
 #' @return 3-column matrix or data frame, with each row representing a feature
 #' @export
 #' @examples
-#'
+#' 
 #' # create a 2-d point cloud of a circle (100 points)
 #' num_pts <- 100
 #' rand_angle <- runif(num_pts, 0, 2 * pi)
@@ -38,11 +38,14 @@ ripser <- function(mat,
                    retval = "mat",
                    use_coef = FALSE) {
 
+  # coerce mat into matrix to work with class object such as dist class object
+  mat <- as.matrix(mat)
+  
   # make sure matrix has at least 2 columns and at least 2 rows
   if (nrow(mat) < 2 | ncol(mat) < 2) {
     stop("Point cloud must have at least 2 points and at least 2 dimensions.")
   }
-
+  
   # make sure matrix contains numeric (or integer) values
   # assumption: matrix can only hold objects of one class so only need to check
   #   one element
@@ -50,19 +53,47 @@ ripser <- function(mat,
   if (class(temp) != "numeric" && class(temp) != "integer") {
     stop("Point cloud must contain values of class `numeric` or `integer` only.")
   }
-
+  
   # make sure there are no NAs in matrix
   if (sum(is.na(mat)) > 0) {
     stop("Point cloud has missing values.")
   }
+  
+  # make sure dim is an integer greater than or equal to zero
+  if (as.integer(dim) != dim) {
+    stop("dim parameter needs to be an integer")
+  }
+  if (dim < 0) {
+    stop("dim cannot be negative")
+  }
+  
+  # make sure threshold is of type numeric
+  if (!(class(threshold) %in% c("numeric", "integer"))) {
+    stop("threshold parameter must be of type numeric")
+  }
+  threshold <- as.numeric(threshold)
+  
+  # make sure format is either "cloud" or "distmat"
+  if (!(format %in% c("cloud", "distmat"))) {
+    stop("format parameter should be either \"cloud\" or \"distmat\"")
+  }
+  int_format <- ifelse(format == "cloud", 0, 1)
 
   # actually do work
-  ans_vec <- ripser_cpp(mat)
+  ans_vec <- ripser_cpp(mat, dim, threshold, int_format)
 
   # format properly and return
   ans_mat <- matrix(ans_vec,
                     byrow = TRUE,
                     ncol = 3)
   colnames(ans_mat) <- c("dimension", "birth", "death")
-  return(ans_mat)
+  
+  # convert matrix to data frame - better for visualization, manipulation
+  if (retval == "df") {
+    ans_df <- as.data.frame(ans_mat)
+    ans_df$dimension <- as.integer(ans_df$dimension)
+    return(ans_df)
+  } else {
+    return(ans_mat)
+  }
 }
