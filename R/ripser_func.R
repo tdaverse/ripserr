67f6febb9d17ebf6ce845ac7f1d3ed1a8b9536d2
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-12-21 11:57:56 -0500

    updated function documentation

--- R/ripser_func.R
+++ R/ripser_func.R
@@ -1,23 +1,42 @@
 #' Calculate Persistent Homology of a Point Cloud
 #'
+#' Calculates the persistent homology of a point cloud, as represented by
+#' a Vietoris-Rips complex. This function is an R wrapper for Ulrich Bauer's
+#' Ripser C++ library for calculating persistent homology. For more
+#' information on the C++ library, see <https://github.com/Ripser/ripser>.
+#'
 #' The `mat` parameter should be a numeric matrix with each row corresponding
 #' to a single point, and each column corresponding to a single dimension. Thus,
-#' if `mat` has 50 rows and 5 columns, it represents a point cloud with 50 points
-#' in 5 dimensions.
+#' if `mat` has 50 rows and 3 columns, it represents a point cloud with 50 points
+#' in 3 dimensions. The `dim` parameter should be a positive integer.
+#' Alternatively, the `mat` parameter could be a distance matrix (upper
+#' triangular half is ignored); note: `format` should be specified as "ldm".
 #'
-#' @param mat numeric matrix containing point cloud
-#' @return 3-column matrix, with each row representing a TDA feature
+#' @param mat numeric matrix containing point cloud or distance matrix
+#' @param dim maximum dimension of features to calculate
+#' @param threshold maximum diameter for computation of Vietoris-Rips complexes
+#' @param format  format of `mat`, either "cloud" for point cloud or "distmat" for distance matrix
+#' @param retval defaults to "mat" (matrix); user can also choose "df" to
+#'   instead return a data frame
+#' @param use_coef logical determining whether support for coefficients in a
+#'   prime field will be enabled
+#' @return 3-column matrix or data frame, with each row representing a feature
 #' @export
 #' @examples
 #'
 #' # create a 2-d point cloud of a circle (100 points)
-#' num.pts <- 100
-#' rand.angle <- runif(num.pts, 0, 2*pi)
-#' pt.cloud <- cbind(cos(rand.angle), sin(rand.angle))
+#' num_pts <- 100
+#' rand_angle <- runif(num_pts, 0, 2 * pi)
+#' pt_cloud <- cbind(cos(rand_angle), sin(rand_angle))
 #'
-#' # calculate persistent homology (num.pts by 3 numeric matrix)
-#' pers.hom <- ripser(pt.cloud)
-ripser <- function(mat) {
+#' # calculate persistent homology (num_pts by 3 numeric matrix)
+#' ripser(pt_cloud)
+ripser <- function(mat,
+                   dim = 1,
+                   threshold = -1,
+                   format = "cloud",
+                   retval = "mat",
+                   use_coef = FALSE) {
 
   # make sure matrix has at least 2 columns and at least 2 rows
   if (nrow(mat) < 2 | ncol(mat) < 2) {
