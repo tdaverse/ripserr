12daaf0c2fb48badc69ce1cead270c4e5189318c
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-15 10:07:12 -0400

    fixed incomplete code using Rcpp

---
+++ R/ripserr.R
@@ -0,0 +1,9 @@
+#' Calculate Persistent Homology with Ripser and Cubical Ripser
+#'
+#' Temp description paragraph; need to replace.
+#'
+#' @useDynLib ripserr
+#' @importFrom Rcpp sourceCpp
+#' @name ripserr
+#' @docType package
+NULL
