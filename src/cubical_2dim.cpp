171be21e0db6eb9bb79a993c32f747f89d237577
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-15 13:13:26 -0400

    added cubicalripser_2dim

--- src/cubical_2dim.cpp
+++ src/cubical_2dim.cpp
@@ -926,8 +926,204 @@ public:
   }
 };
 
-// placeholder for compilation to work - REMOVE once cubical_2dim is fully ported
-int main()
+/*****cubicalripser_2dim*****/
+enum calculation_method
 {
+  LINKFIND,
+  COMPUTEPAIRS
+};
+
+void print_usage_and_exit(int exit_code)
+{
+  cerr << "Usage: "
+       << "CR2 "
+       << "[options] [input_filename]" << endl
+       << endl
+       << "Options:" << endl
+       << endl
+       << "  --help           print this screen" << endl
+       << "  --format         use the specified file format for the input. Options are:" << endl
+       << "                     dipha          (pixel data in DIPHA file format; default)" << endl
+       << "                     perseus        (pixel data in Perseus file format)" << endl
+       << "  --threshold <t>  compute cubical complexes up to birth time <t>" << endl
+       << "  --method         method to compute the persistent homology of the cubical complexes. Options are" << endl
+       << "                     link_find      (calculating the 0-dim PP, use 'link_find' algorithm; default)" << endl
+       << "                     compute_pairs  (calculating the 0-dim PP, use 'compute_pairs' algorithm)" << endl
+       << "  --output         name of file that will contain the persistence diagram " << endl
+       << "  --print          print persistence pairs on your console" << endl
+       << endl;
+
+  exit(exit_code);
+}
+
+// placeholder for compilation to work - RENAME w/ Rcpp stuff once cubical_2dim is fully ported
+int main(int argc, char** argv)
+{
+  const char* filename = nullptr;
+  string output_filename = "answer_2dim.diagram"; //default name
+  file_format format = DIPHA;
+  calculation_method method = LINKFIND;
+  double threshold = 99999;
+  bool print = false;
+
+  for (int i = 1; i < argc; ++i)
+  {
+    const string arg(argv[i]);
+    if (arg == "--help")
+    {
+      print_usage_and_exit(0);
+    }
+    else if (arg == "--threshold")
+    {
+      string parameter = string(argv[++i]);
+      size_t next_pos;
+      threshold = stod(parameter, &next_pos);
+      if (next_pos != parameter.size())
+        print_usage_and_exit(-1);
+    }
+    else if (arg == "--format")
+    {
+      string parameter = string(argv[++i]);
+      if (parameter == "dipha")
+      {
+        format = DIPHA;
+      }
+      else if (parameter == "perseus")
+      {
+        format = PERSEUS;
+      }
+      else
+      {
+        print_usage_and_exit(-1);
+      }
+    }
+    else if(arg == "--method")
+    {
+      string parameter = string(argv[++i]);
+      if (parameter == "link_find")
+      {
+        method = LINKFIND;
+      }
+      else if (parameter == "compute_pairs")
+      {
+        method = COMPUTEPAIRS;
+      }
+      else
+      {
+        print_usage_and_exit(-1);
+      }
+    }
+    else if (arg == "--output")
+    {
+      output_filename = string(argv[++i]);
+    }
+    else if(arg == "--print")
+    {
+      print = true;
+    }
+    else
+    {
+      if (filename)
+      {
+        print_usage_and_exit(-1);
+      }
+      filename = argv[i];
+    }
+  }
+
+  ifstream file_stream(filename);
+  if (filename && file_stream.fail())
+  {
+    cerr << "couldn't open file " << filename << std::endl;
+    exit(-1);
+  }
+
+  vector<WritePairs> writepairs; // dim birth death
+  writepairs.clear();
+
+  DenseCubicalGrids* dcg = new DenseCubicalGrids(filename, threshold, format);
+  ColumnsToReduce* ctr = new ColumnsToReduce(dcg);
+
+  switch(method)
+  {
+    case LINKFIND:
+    {
+      JointPairs* jp = new JointPairs(dcg, ctr, writepairs, print);
+      jp->joint_pairs_main(); // dim0
+
+      ComputePairs* cp = new ComputePairs(dcg, ctr, writepairs, print);
+      cp->compute_pairs_main(); // dim1
+
+      break;
+    }
+
+    case COMPUTEPAIRS:
+    {
+      ComputePairs* cp = new ComputePairs(dcg, ctr, writepairs, print);
+      cp->compute_pairs_main(); // dim0
+      cp->assemble_columns_to_reduce();
+
+      cp->compute_pairs_main(); // dim1
+
+      break;
+    }
+  }
+
+
+#ifdef FILE_OUTPUT
+  ofstream writing_file;
+
+  string extension = ".csv";
+  if (equal(extension.rbegin(), extension.rend(), output_filename.rbegin()) == true)
+  {
+    string outname = output_filename;
+    writing_file.open(outname, ios::out);
+
+    if(!writing_file.is_open())
+    {
+      cout << " error: open file for output failed! " << endl;
+    }
+
+    int64_t p = writepairs.size();
+    for (int64_t i = 0; i < p; ++i)
+    {
+      writing_file << writepairs[i].getDimension() << ",";
+
+      writing_file << writepairs[i].getBirth() << ",";
+      writing_file << writepairs[i].getDeath() << endl;
+    }
+    writing_file.close();
+  }
+  else
+  {
+    writing_file.open(output_filename, ios::out | ios::binary);
+
+    if (!writing_file.is_open())
+    {
+      cout << " error: open file for output failed! " << endl;
+    }
+
+    int64_t mn = 8067171840;
+    writing_file.write((char*) &mn, sizeof(int64_t)); // magic number
+    int64_t type = 2;
+    writing_file.write((char*) &type, sizeof(int64_t)); // type number of PERSISTENCE_DIAGRAM
+    int64_t p = writepairs.size();
+    cout << "the number of pairs : " << p << endl;
+    writing_file.write((char*) &p, sizeof(int64_t)); // number of points in the diagram p
+    for(int64_t i = 0; i < p; ++i)
+    {
+      int64_t writedim = writepairs[i].getDimension();
+      writing_file.write((char*) &writedim, sizeof(int64_t)); // dim
+
+      double writebirth = writepairs[i].getBirth();
+      writing_file.write((char*) &writebirth, sizeof(double)); // birth
+
+      double writedeath = writepairs[i].getDeath();
+      writing_file.write((char*) &writedeath, sizeof(double)); // death
+    }
+    writing_file.close();
+  }
+#endif
+
   return 0;
 }
