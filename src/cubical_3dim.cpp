b6a39d6adfb338135a0d17bfec50ea7d32a9e08c
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-16 17:06:22 -0400

    added joint_pairs

--- src/cubical_3dim.cpp
+++ src/cubical_3dim.cpp
@@ -912,6 +912,122 @@ public:
 };
 
 /*****joint_pairs*****/
+class JointPairs
+{
+  // member vars
+  int n; // the number of cubes
+  int ctr_moi;
+  int ax, ay, az;
+  DenseCubicalGrids* dcg;
+  ColumnsToReduce* ctr;
+  vector<WritePairs> *wp;
+  bool print;
+  Vertices* vtx;
+  double u, v;
+  vector<int64_t> cubes_edges;
+  vector<BirthdayIndex> dim1_simplex_list;
+
+public:
+  // constructor
+  JointPairs(DenseCubicalGrids* _dcg, ColumnsToReduce* _ctr, vector<WritePairs> &_wp, const bool _print)
+  {
+    dcg = _dcg;
+    ax = dcg -> ax;
+    ay = dcg -> ay;
+    az = dcg -> az;
+    ctr = _ctr; // ctr is "0-dim"simplex list.
+    ctr_moi = ctr->max_of_index;
+    n = ctr->columns_to_reduce.size();
+    print = _print;
+
+    wp = &_wp;
+    vtx = new Vertices();
+
+    for (int x = 1; x <= ax; ++x)
+    {
+      for (int y = 1; y <= ay; ++y)
+      {
+        for (int z = 1; z <= az; ++z)
+        {
+          for (int type = 0; type < 3; ++type)
+          {
+            int index = x | (y << 9) | (z << 18) | (type << 27);
+            double birthday = dcg -> getBirthday(index, 1);
+            if (birthday < dcg -> threshold)
+            {
+              dim1_simplex_list.push_back(BirthdayIndex(birthday, index, 1));
+            }
+          }
+        }
+      }
+    }
+    sort(dim1_simplex_list.rbegin(), dim1_simplex_list.rend(), BirthdayIndexComparator());
+  }
+
+  // member method - workhorse
+  void joint_pairs_main()
+  {
+    cubes_edges.reserve(2);
+    UnionFind dset(ctr_moi, dcg);
+    ctr->columns_to_reduce.clear();
+    ctr->dim = 1;
+    double min_birth = dcg->threshold;
+
+    if (print == true)
+    {
+      cout << "persistence intervals in dim " << 0 << ":" << endl;
+    }
+
+    for (auto e : dim1_simplex_list)
+    {
+      cubes_edges.clear();
+      dcg->GetSimplexVertices(e.getIndex(), 1, vtx);
+
+      cubes_edges[0] = vtx->vertex[0]->getIndex();
+      cubes_edges[1] = vtx->vertex[1]->getIndex();
+
+      u = dset.find(cubes_edges[0]);
+      v = dset.find(cubes_edges[1]);
+
+      if (min_birth >= min(dset.birthtime[u], dset.birthtime[v]))
+      {
+        min_birth = min(dset.birthtime[u], dset.birthtime[v]);
+      }
+
+      if (u != v)
+      {
+        double birth = max(dset.birthtime[u], dset.birthtime[v]);
+        double death = max(dset.time_max[u], dset.time_max[v]);
+
+        if (birth == death)
+        {
+          dset.link(u, v);
+        }
+        else
+        {
+          if (print == true)
+          {
+            cout << "[" << birth << "," << death << ")" << endl;
+          }
+          wp->push_back(WritePairs(0, birth, death));
+          dset.link(u, v);
+        }
+      }
+      else // If two values have same "parent", these are potential edges which make a 2-simplex.
+      {
+        ctr->columns_to_reduce.push_back(e);
+      }
+    }
+
+    if (print == true)
+    {
+      cout << "[" << min_birth << ", )" << endl;
+    }
+
+    wp->push_back(WritePairs(-1, min_birth, dcg -> threshold));
+    sort(ctr->columns_to_reduce.begin(), ctr->columns_to_reduce.end(), BirthdayIndexComparator());
+  }
+};
 
 /*****compute_pairs*****/
 
