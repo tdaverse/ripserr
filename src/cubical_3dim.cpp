8ec8f0ce5f675cbdde740ee13e18bfe30c94ec85
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-16 15:24:21 -0400

    template for cubical_3dim

---
+++ src/cubical_3dim.cpp
@@ -0,0 +1,50 @@
+/*
+ This file is an altered form of the Cubical Ripser software created by
+ Takeki Sudo and Kazushi Ahara. Details of the original software are below the
+ dashed line.
+ -Raoul Wadhwa
+ -------------------------------------------------------------------------------
+ Copyright 2017-2018 Takeki Sudo and Kazushi Ahara.
+ This file is part of CubicalRipser_2dim.
+ CubicalRipser: C++ system for computation of Cubical persistence pairs
+ Copyright 2017-2018 Takeki Sudo and Kazushi Ahara.
+ CubicalRipser is free software: you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or (at your option)
+ any later version.
+ CubicalRipser is deeply depending on 'Ripser', software for Vietoris-Rips
+ persitence pairs by Ulrich Bauer, 2015-2016.  We appreciate Ulrich very much.
+ We rearrange his codes of Ripser and add some new ideas for optimization on it
+ and modify it for calculation of a Cubical filtration.
+ This part of CubicalRiper is a calculator of cubical persistence pairs for
+ 2 dimensional pixel data. The input data format conforms to that of DIPHA.
+ See more descriptions in README.
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public License along
+ with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*****birthday_index*****/
+
+/*****coeff*****/
+
+/*****write_pairs*****/
+
+/*****vertices*****/
+
+/*****dense_cubical_grids*****/
+
+/*****columns_to_reduce*****/
+
+/*****union_find*****/
+
+/*****simplex_coboundary_enumerator*****/
+
+/*****joint_pairs*****/
+
+/*****compute_pairs*****/
+
+/*****cubicalripser_3dim*****/
