02c607a4d1210a825f22cfdb19f60c84565ddc83
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-16 16:29:26 -0400

    added columns_to_reduce 3dim

--- src/cubical_3dim.cpp
+++ src/cubical_3dim.cpp
@@ -29,6 +29,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <algorithm>
 #include <cstdint>
 #include <cassert>
@@ -512,6 +513,49 @@ public:
 };
 
 /*****columns_to_reduce*****/
+class ColumnsToReduce
+{
+  // member vars
+public:
+  vector<BirthdayIndex> columns_to_reduce;
+  int dim;
+  int max_of_index;
+
+  // constructor
+  ColumnsToReduce(DenseCubicalGrids* _dcg)
+  {
+    dim = 0;
+    int ax = _dcg->ax;
+    int ay = _dcg->ay;
+    int az = _dcg->az;
+    max_of_index = 512 * 512 * (az + 2);
+    int index;
+    double birthday;
+
+    for (int z = az; z > 0; --z)
+    {
+      for (int y = ay; y > 0; --y)
+      {
+        for (int x = ax; x > 0; --x)
+        {
+          birthday = _dcg->dense3[x][y][z];
+          index = x | (y << 9) | (z << 18);
+          if (birthday != _dcg->threshold)
+          {
+            columns_to_reduce.push_back(BirthdayIndex(birthday, index, 0));
+          }
+        }
+      }
+    }
+    sort(columns_to_reduce.begin(), columns_to_reduce.end(), BirthdayIndexComparator());
+  }
+
+  // getter (length of vector)
+  int size()
+  {
+    return columns_to_reduce.size();
+  }
+};
 
 /*****union_find*****/
 
