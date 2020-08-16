6afb33f4fe4692cb32f13e1a3c2cf7105ad14249
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-16 16:33:41 -0400

    added union_find 3dim

--- src/cubical_3dim.cpp
+++ src/cubical_3dim.cpp
@@ -558,6 +558,73 @@ public:
 };
 
 /*****union_find*****/
+class UnionFind
+{
+  // member vars
+public:
+  int max_of_index;
+  vector<int> parent;
+  vector<double> birthtime;
+  vector<double> time_max;
+  DenseCubicalGrids* dcg;
+
+  // constructor
+  UnionFind(int moi, DenseCubicalGrids* _dcg) : parent(moi), birthtime(moi), time_max(moi) // Thie "n" is the number of cubes.
+  {
+    dcg = _dcg;
+    max_of_index = moi;
+
+    for (int i = 0; i < moi; ++i)
+    {
+      parent[i] = i;
+      birthtime[i] = dcg->getBirthday(i, 0);
+      time_max[i] = dcg->getBirthday(i, 0);
+    }
+  }
+
+  // member methods
+  int find(int x) // Thie "x" is Index.
+  {
+    int y = x, z = parent[y];
+    while (z != y)
+    {
+      y = z;
+      z = parent[y];
+    }
+    y = parent[x];
+    while (z != y)
+    {
+      parent[x] = z;
+      x = y;
+      y = parent[x];
+    }
+    return z;
+  }
+
+  void link(int x, int y)
+  {
+    x = find(x);
+    y = find(y);
+    if (x == y) return;
+    if (birthtime[x] > birthtime[y])
+    {
+      parent[x] = y;
+      birthtime[y] = min(birthtime[x], birthtime[y]);
+      time_max[y] = max(time_max[x], time_max[y]);
+    }
+    else if (birthtime[x] < birthtime[y])
+    {
+      parent[y] = x;
+      birthtime[x] = min(birthtime[x], birthtime[y]);
+      time_max[x] = max(time_max[x], time_max[y]);
+    }
+    else //birthtime[x] == birthtime[y]
+    {
+      parent[x] = y;
+      time_max[y] = max(time_max[x], time_max[y]);
+    }
+  }
+};
 
 /*****simplex_coboundary_enumerator*****/
 
