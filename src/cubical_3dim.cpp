9edf7b312b4b9511390b72b35c143bee6cad17c2
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-16 17:22:13 -0400

    added compute_pairs 3dim

--- src/cubical_3dim.cpp
+++ src/cubical_3dim.cpp
@@ -29,7 +29,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <queue>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 #include <cstdint>
 #include <cassert>
@@ -1030,6 +1032,261 @@ public:
 };
 
 /*****compute_pairs*****/
+template <class Key, class T> class hash_map : public std::unordered_map<Key, T> {};
+
+class ComputePairs
+{
+  // member vars
+public:
+  DenseCubicalGrids* dcg;
+  ColumnsToReduce* ctr;
+  hash_map<int, int> pivot_column_index;
+  int ax, ay, az;
+  int dim;
+  vector<WritePairs> *wp;
+  bool print;
+
+  // constructor
+  ComputePairs(DenseCubicalGrids* _dcg, ColumnsToReduce* _ctr, vector<WritePairs> &_wp, const bool _print)
+  {
+    dcg = _dcg;
+    ctr = _ctr;
+    dim = _ctr->dim;
+    wp = &_wp;
+    print = _print;
+
+    ax = _dcg->ax;
+    ay = _dcg->ay;
+    az = _dcg->az;
+  }
+
+  // member methods
+  void compute_pairs_main()
+  {
+    if (print == true)
+    {
+      cout << "persistence intervals in dim " << dim << ":" << endl;
+    }
+
+    pivot_column_index = hash_map<int, int>();
+    vector<BirthdayIndex> coface_entries;
+    auto ctl_size = ctr->columns_to_reduce.size();
+    SimplexCoboundaryEnumerator cofaces;
+    unordered_map<int, priority_queue<BirthdayIndex, vector<BirthdayIndex>, BirthdayIndexComparator>> recorded_wc;
+
+    pivot_column_index.reserve(ctl_size);
+    recorded_wc.reserve(ctl_size);
+
+    for (int i = 0; i < ctl_size; ++i)
+    {
+      auto column_to_reduce = ctr->columns_to_reduce[i];
+      priority_queue<BirthdayIndex, vector<BirthdayIndex>, BirthdayIndexComparator> working_coboundary;
+      double birth = column_to_reduce.getBirthday();
+
+      int j = i;
+      BirthdayIndex pivot(0, -1, 0);
+      bool might_be_apparent_pair = true;
+      bool goto_found_persistence_pair = false;
+
+      do {
+        auto simplex = ctr->columns_to_reduce[j]; // get CTR[i]
+        coface_entries.clear();
+        cofaces.setSimplexCoboundaryEnumerator(simplex, dcg);// make coface data
+
+        while (cofaces.hasNextCoface() && !goto_found_persistence_pair) // repeat there remains a coface
+        {
+          BirthdayIndex coface = cofaces.getNextCoface();
+          coface_entries.push_back(coface);
+          if (might_be_apparent_pair && (simplex.getBirthday() == coface.getBirthday())) // If bt is the same, go thru
+          {
+            if (pivot_column_index.find(coface.getIndex()) == pivot_column_index.end()) // If coface is not in pivot list
+            {
+              pivot.copyBirthdayIndex(coface); // I have a new pivot
+              goto_found_persistence_pair = true; // goto (B)
+            }
+            else // If pivot list contains this coface,
+            {
+              might_be_apparent_pair = false; // goto (A)
+            }
+          }
+        }
+
+        if (!goto_found_persistence_pair) // (A) If pivot list contains this coface,
+        {
+          auto findWc = recorded_wc.find(j); // we seek wc list by 'j'
+
+          if (findWc != recorded_wc.end()) // If the pivot is old,
+          {
+            auto wc = findWc->second;
+            while(!wc.empty()) // we push the data of the old pivot's wc
+            {
+              auto e = wc.top();
+              working_coboundary.push(e);
+              wc.pop();
+            }
+          }
+          else // If the pivot is new,
+          {
+            for(auto e : coface_entries) // making wc here
+            {
+              working_coboundary.push(e);
+            }
+          }
+          pivot = get_pivot(working_coboundary); // getting a pivot from wc
+
+          if (pivot.getIndex() != -1) // When I have a pivot, ...
+          {
+            auto pair = pivot_column_index.find(pivot.getIndex());
+            if (pair != pivot_column_index.end())	// If the pivot already exists, go on the loop
+            {
+              j = pair->second;
+              continue;
+            }
+            else // If the pivot is new,
+            {
+              // I record this wc into recorded_wc, and
+              recorded_wc.insert(make_pair(i, working_coboundary));
+              // I output PP as Writepairs
+              double death = pivot.getBirthday();
+              outputPP(dim, birth, death);
+              pivot_column_index.insert(make_pair(pivot.getIndex(), i));
+              break;
+            }
+          }
+          else // If wc is empty, I output a PP as [birth,)
+          {
+            outputPP(-1, birth, dcg->threshold);
+            break;
+          }
+        }
+        else // (B) I have a new pivot and output PP as Writepairs
+        {
+          double death = pivot.getBirthday();
+          outputPP(dim, birth, death);
+          pivot_column_index.insert(make_pair(pivot.getIndex(), i));
+          break;
+        }
+      } while (true);
+    }
+  }
+
+  void outputPP(int _dim, double _birth, double _death)
+  {
+    if (_birth != _death)
+    {
+      if (_death != dcg->threshold)
+      {
+        if (print == true)
+        {
+          cout << "[" <<_birth << "," << _death << ")" << endl;
+        }
+        wp->push_back(WritePairs(_dim, _birth, _death));
+      }
+      else
+      {
+        if (print == true)
+        {
+          cout << "[" << _birth << ", )" << endl;
+        }
+        wp->push_back(WritePairs(-1, _birth, dcg->threshold));
+      }
+    }
+  }
+
+  BirthdayIndex pop_pivot(priority_queue<BirthdayIndex, vector<BirthdayIndex>, BirthdayIndexComparator>& column)
+  {
+    if (column.empty())
+    {
+      return BirthdayIndex(0, -1, 0);
+    }
+    else
+    {
+      auto pivot = column.top();
+      column.pop();
+
+      while (!column.empty() && column.top().index == pivot.getIndex())
+      {
+        column.pop();
+        if (column.empty())
+          return BirthdayIndex(0, -1, 0);
+        else
+        {
+          pivot = column.top();
+          column.pop();
+        }
+      }
+      return pivot;
+    }
+  }
+
+  BirthdayIndex get_pivot(priority_queue<BirthdayIndex, vector<BirthdayIndex>, BirthdayIndexComparator>& column)
+  {
+    BirthdayIndex result = pop_pivot(column);
+    if (result.getIndex() != -1)
+    {
+      column.push(result);
+    }
+    return result;
+  }
+
+  void assemble_columns_to_reduce()
+  {
+    ++dim;
+    ctr->dim = dim;
+
+    if (dim == 1)
+    {
+      ctr->columns_to_reduce.clear();
+      for (int z = 1; z <= az; ++z)
+      {
+        for (int y = 1; y <= ay; ++y)
+        {
+          for (int x = 1; x <= ax; ++x)
+          {
+            for (int m = 0; m < 3; ++m) // the number of type
+            {
+              double index = x | (y << 9) | (z << 18) | (m << 27);
+              if (pivot_column_index.find(index) == pivot_column_index.end())
+              {
+                double birthday = dcg->getBirthday(index, 1);
+                if (birthday != dcg->threshold)
+                {
+                  ctr->columns_to_reduce.push_back(BirthdayIndex(birthday, index, 1));
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    else if (dim == 2)
+    {
+      ctr->columns_to_reduce.clear();
+      for (int z = 1; z <= az; ++z)
+      {
+        for (int y = 1; y <= ay; ++y)
+        {
+          for (int x = 1; x <= ax; ++x)
+          {
+            for (int m = 0; m < 3; ++m) // the number of type
+            {
+              double index = x | (y << 9) | (z << 18) | (m << 27);
+              if (pivot_column_index.find(index) == pivot_column_index.end())
+              {
+                double birthday = dcg->getBirthday(index, 2);
+                if (birthday != dcg->threshold)
+                {
+                  ctr->columns_to_reduce.push_back(BirthdayIndex(birthday, index, 2));
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    sort(ctr->columns_to_reduce.begin(), ctr->columns_to_reduce.end(), BirthdayIndexComparator());
+  }
+};
 
 /*****cubicalripser_3dim*****/
 //PLACEHOLDER - WILL REPLACE
