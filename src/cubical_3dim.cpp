53c42a0d8c9d716661d247db28659bade7a1448c
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-16 15:34:03 -0400

    added birthday_index 3dim

--- src/cubical_3dim.cpp
+++ src/cubical_3dim.cpp
@@ -26,8 +26,138 @@
  with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <iostream>
+
+using namespace std;
 
 /*****birthday_index*****/
+class BirthdayIndex
+{
+  // member vars
+public:
+  double birthday;
+  int index;
+  int dim;
+
+  // default empty constructor
+  BirthdayIndex()
+  {
+    birthday = 0;
+    index = -1;
+    dim = 1;
+  }
+
+  // member setter constructor
+  BirthdayIndex(double _b, int _i, int _d)
+  {
+    birthday = _b;
+    index = _i;
+    dim = _d;
+  }
+
+  // clone/copy constructor
+  BirthdayIndex(const BirthdayIndex& b)
+  {
+    birthday = b.birthday;
+    index = b.index;
+    dim = b.dim;
+  }
+
+  // clone/copy method
+  void copyBirthdayIndex(BirthdayIndex v)
+  {
+    birthday = v.birthday;
+    index = v.index;
+    dim = v.dim;
+  }
+
+  // getters
+  double getBirthday()
+  {
+    return birthday;
+  }
+  long getIndex()
+  {
+    return index;
+  }
+  int getDimension()
+  {
+    return dim;
+  }
+
+  // output methods
+  void print()
+  {
+    cout << "(dob:" << birthday << "," << index << ")" << endl;
+  }
+  void VertexPrint()
+  {
+    int px = index & 0x01ff;
+    int py = (index >> 9) & 0x01ff;
+    int pz = (index >> 18) & 0x01ff;
+    int pm = (index >> 27) & 0xff;
+
+    cout << "birthday : (m, z, y, x) = " << birthday << " : (" << pm << ", " << pz << ", " << py << ", " << px << ")" << endl;
+  }
+};
+
+struct BirthdayIndexComparator
+{
+  bool operator()(const BirthdayIndex& o1, const BirthdayIndex& o2) const
+  {
+    if (o1.birthday == o2.birthday)
+    {
+      if (o1.index < o2.index)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    else
+    {
+      if (o1.birthday > o2.birthday)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+};
+
+struct BirthdayIndexInverseComparator
+{
+  bool operator()(const BirthdayIndex& o1, const BirthdayIndex& o2) const
+  {
+    if (o1.birthday == o2.birthday)
+    {
+      if (o1.index < o2.index)
+      {
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+    }
+    else
+    {
+      if (o1.birthday > o2.birthday)
+      {
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+    }
+  }
+};
 
 /*****coeff*****/
 
@@ -48,3 +178,8 @@
 /*****compute_pairs*****/
 
 /*****cubicalripser_3dim*****/
+//PLACEHOLDER - WILL REPLACE
+int main()
+{
+  return 0;
+}
