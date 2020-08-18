6d96ef74e4ec3b81ebd2155f2addf6199261a2fb
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-18 15:52:19 -0400

    added test for cubical_4dim

--- tests/testthat/test-cubical.R
+++ tests/testthat/test-cubical.R
@@ -1,7 +1,7 @@
-context("2-dimensional cubical complexes")
+context("cubical")
 library("ripserr")
 
-test_that("basic function works", {
+test_that("basic 2-dim cubical works", {
   # reproducibility
   set.seed(42)
   
@@ -23,4 +23,20 @@ test_that("basic function works", {
   expect_equal(counts[1], 1)
   expect_equal(counts[2], 20)
   expect_equal(counts[3], 7)
+})
+
+test_that("basic 4-dim cubical works", {
+  # reproducibility
+  set.seed(42)
+  
+  # create data
+  test_data <- rnorm(5 * 5 * 5 * 5, mean = 1000, sd = 150)
+  dim(test_data) <- c(5, 5, 5, 5)
+  
+  # create cubical complex
+  cub_comp <- ripserr::cubical(test_data)
+  
+  # test cubical complex
+  expect_equal(ncol(cub_comp), 3)
+  expect_equal(nrow(cub_comp), 1681) ## WILL CHANGE WHEN BUG IN CUBICAL_4DIM IS FIXED
 })
