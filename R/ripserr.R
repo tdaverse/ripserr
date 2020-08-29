039d3f4ad6cc993eaa215211a7ba43dd27aa0a7c
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-08-29 11:42:13 -0400

    adjusted title

--- R/ripserr.R
+++ R/ripserr.R
@@ -1,4 +1,4 @@
-#' Calculate Persistent Homology with Ripser and Cubical Ripser
+#' Calculate Persistent Homology with Ripser-Based Engines
 #'
 #' Ports Ripser-based persistent homology calculation engines
 #' from C++ to R using the Rcpp package.
