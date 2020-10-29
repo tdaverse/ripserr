64be9f34cef2ed9b2bbde8cf6b643b101d05fd10
Raoul W <raoulwadhwa@gmail.com>
2020-10-29 16:51:11 -0400

    filled out s3 methods

--- R/calc_s3.R
+++ R/calc_s3.R
@@ -1,5 +1,6 @@
 # s3 generic
-#' @export
+#' @rdname calc_phom
+#' @export calc_phom
 calc_phom <- function(x, ...) {
   UseMethod("calc_phom")
 }
@@ -9,11 +10,13 @@ calc_phom <- function(x, ...) {
 #     Umeda Y. Time Series Classification via Topological Data Analysis.
 #     Transactions of the Japanese Society for Artificial Intelligence. 2017;
 #     32(3): DG72 1-12. doi: 10.1527/tjsai.D-G72
+#' @rdname calc_phom
+#' @export calc_phom.numeric
 #' @export
 calc_phom.numeric <- function(x,
                               data_dim = 2L, max_dim = 1L,
                               initial_term = 1L,
-                              smp_lag = 1) {
+                              smp_lag = 1L) {
   
   seq_index <- seq(from = initial_term,
                    by = smp_lag,
@@ -30,35 +33,73 @@ calc_phom.numeric <- function(x,
   
   tmp_df <- as.data.frame(tmp_mat)
   
-  return(calc_phom.data.frame(tmp_df))
+  return(ripserr::calc_phom.data.frame(tmp_df))
 }
 
 # time series objects (class ts; include xts, zoo, etc. in TDAstats)
+# switch quasi-attractor code from numeric to ts; then add param method "quasi" to ts
+#' @rdname calc_phom
+#' @export calc_phom.ts
+#' @export
 calc_phom.ts <- function(x,
                          data_dim = 2L, max_dim = 1L,
-                         lags = base::seq_len(data_dim - 1)) {
+                         initial_term = 1L,
+                         smp_lag = 1L) {
   # convert ts object to appropriately formatted data frame
-  intermediary <- data.frame()
+  intermediary <- as.numeric(x)
+  
+  #####INSERT HERE#####
   
   # calculate using calc_phom.data.frame
-  return(calc_phom.data.frame(intermediary))
+  return(ripserr::calc_phom.numeric(intermediary,
+                                    data_dim = data_dim,
+                                    max_dim = max_dim,
+                                    initial_term = initial_term,
+                                    smp_lag = smp_lag))
+}
+
+# for dist objects
+# can only be vietoris_rips
+#' @rdname calc_phom
+#' @export calc_phom.dist
+#' @export
+calc_phom.dist <- function(x,
+                           max_dim = 1L, threshold = -1, p = 2L,
+                           standardize = FALSE, return_format = "df") {
+  ripserr::vietoris_rips(dataset = x,
+                         dim = max_dim,
+                         threshold = threshold,
+                         p = p,
+                         standardize = standardize,
+                         return_format = return_format)
 }
 
 # data frames (include tibble in TDAstats)
 # can only be vietoris_rips (2d cubical should be in array/matrix format)
+#' @rdname calc_phom
+#' @export calc_phom.data.frame
 #' @export
 calc_phom.data.frame <- function(x,
                                  max_dim = 1L, threshold = -1, p = 2L,
                                  standardize = FALSE, return_format = "df") {
-    ripserr::vietoris_rips(dataset = x,
-                           dim = max_dim,
-                           threshold = threshold,
-                           p = p,
-                           standardize = standardize,
-                           return_format = return_format)
+  ripserr::vietoris_rips(dataset = x,
+                         dim = max_dim,
+                         threshold = threshold,
+                         p = p,
+                         standardize = standardize,
+                         return_format = return_format)
 }
 
 # arrays (includes matrix)
-calc_phom.array <- function() {
-  
+#' @rdname calc_phom
+#' @export calc_phom.array
+#' @export
+calc_phom.array <- function(x,
+                            threshold = 9999, method = 0,
+                            standardize = FALSE, return_format = "df") {
+  ripserr::cubical(dataset = x,
+                   threshold = threshold,
+                   method = method,
+                   standardize = standardize,
+                   return_format = return_format)
 }
