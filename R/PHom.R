2acd8ad74c4da16fd45f938e89f28a7efdf1a243
Raoul W <raoulwadhwa@gmail.com>
2020-11-15 14:03:36 -0500

    basic PHom constructor

--- R/PHom.R
+++ R/PHom.R
@@ -1,1 +1,30 @@
-# S3 class for basic persistence data
+# PHom: S3 class for basic persistence data (PHom objects should also have "data.frame" %in% class)
+# Structure of data = set of features, each of which has:
+# - dimension <int>
+# - birth <dbl>
+# - death <dbl>
+
+#####BACKEND BASICS#####
+new_PHom <- function(x = data.frame(dimension = integer(0),
+                                    birth = double(0),
+                                    death = double(0)),
+                     dim_col = "dimension",
+                     b_col = "birth",
+                     d_col = "death") {
+  # ensure valid parameters
+  
+  # construct df for PHom object
+  ans <- data.frame(dimension = as.integer(x[, dim_col]),
+                    birth = as.double(x[, b_col]),
+                    death = as.double(x[, d_col]))
+    
+  # structure class appropriately
+  structure(ans,
+            class = c("PHom", "data.frame"))
+}
+
+#####HELPER#####
+
+#####CONVERTER/CHECKER#####
+
+#####S3 GENERICS#####
