88aa757873c98d3e7f6f8509ea0efe9a0ddf756e
Brynne Farrell <brynne.farrell@gmail.com>
2021-01-24 10:33:32 -0600

    Added function documentation.

--- data-raw/geocode_data/geocode.r
+++ data-raw/geocode_data/geocode.r
@@ -35,6 +35,18 @@ brazilStateCodeLookup <- hash(
 # Set the default sleep delay as required by the Nominatim API documentation to not make more than one request per second.
 nominatimSleepDelay = 1.1
 
+#' Reverse geocode a coordinate pair.
+#' 
+#' This converts the coordinates into state and country names.
+#' 
+#' @param longitude Longitude of the coordinate. Sometimes labeled as the "X" coordinate.
+#' @param latitude Latitude of the coordinate. Sometimes labeled as the "Y" coordinate.
+#' @param email Email address of the person running this script. Required by the Nominatim API Terms of Service.
+#' 
+#' @return This will return a list with names "state_name", "state_code", "country_name", and "country_code"
+#' 
+#' @examples
+#' reverseGeocode(-32.21, -52.38, "test@@asdf.com")
 reverseGeocode <- function (longitude, latitude, email) 
 {
   # Pause as required by the Nominatim API.
@@ -69,6 +81,18 @@ reverseGeocode <- function (longitude, latitude, email)
   return(result)
 }
 
+
+#' Geocode every row in a file.
+#' 
+#' For demonstration purposes here, only rows that have the field "COUNTRY" set 
+#' to "Brasil" will be geocoded. All others will be ignored.
+#'
+#' @param inputFilePath CSV file to read lat/lon data from.
+#' @param outputFilePath CSV file path to write geocoded results to. If this file exists, it will be deleted.
+#' @param email Email address of the person running this script. Required by the Nominatim API Terms of Service.
+#' 
+#' @examples
+#' processFile("path/to/input.csv", "path/to/output.csv", "test@@asdf.com")
 processFile <- function(inputFilePath, outputFilePath, email)
 {
   print("Reverse geocoding has started.")
@@ -126,6 +150,9 @@ processFile <- function(inputFilePath, outputFilePath, email)
   print("Reverse geocoding has completed.")
 }
 
+#' Run the script in interactive mode.
+#' 
+#' Interactive mode will prompt the user for input files and email.
 executeInteractiveScript <- function() {
   defaultdownloadurl = "https://uc3-s3mrt1001-prd.s3.us-west-2.amazonaws.com/3877a3a1-b8e2-4c47-9a55-a03572325842/data?response-content-disposition=attachment%3b%20filename%3ddoi_10.5061_dryad.47v3c__v2.zip&response-content-type=application%2fzip&x-amz-security-token=iqojb3jpz2lux2vjeakacxvzlxdlc3qtmijgmeqciblms%2biaicmw%2bvtf3t1ywxfkrewykec%2b6csg2qd7rfq4aibssye%2fqdipxrayzrbfrfzaaeofoiovyiclxhfldvmoncq9awji%2f%2f%2f%2f%2f%2f%2f%2f%2f%2f8beaaaddq1mtgynjkxnde1nyimm3ggolpzburlhpuckpedmwo5kddo1adxqnytewmfuz2svn0redp927zd30da2ec3zba9avwi33%2fwxr2arvd4vwn%2be%2bibfv9o6hhmpzvl91us9o2j6txctic1ok%2fqax%2bnzu1nc1tgmsxr6rqj5aaerpqizlx5my1fqclfldw%2bbi035meqhcjmnv%2fykklr0itxx%2fsan0jqi55vl2u2lh1b%2fyyplx5rd5hdi1%2brxmgqjckuhelacdt2yshhsteynz%2buqoox6wtdz2qh%2f%2fvk3vsorll%2blbfz9ktncy0vvhalr4yvhdy8pvyvwrz415idy7qp0wppkioq1k%2bj5p9qc8stfo53sivzlzxxdofdy8ir84y0qz9dfel4avimm2oqht8aaubhvfjkzsh0mfefqkp%2bvdfebj1wmrkkio4ng8lczrwbngnksxzewu1fopujnqdhashzpwtftan9vy7v494fchqu9g%2flj94ndvpnkxa8ls1gqhct1tawhzbuilwm9abwjedeekprswtpnbnwdhnf61kj3ysrfkioozuqvrimiqkwjjgzgay67ahcocftdmsye3fdn9fixittzp6v%2bydy4bzju63gncyrlei1vwr%2b5ispdemztaudzge3lxsse%2bclaeic3izgn6qbn16fplgi%2bfwb9lold%2bglcecyh0wice7mqqns5lwkgbnqvztciaphpkkzxumpemhzkduz97mgds5yip%2f2b2yt3xklvuedju9b%2bka24n2q%2bwihtsrrbd8cq738ev6op12wkygmbe7r7wmhywpgcm1kxibk6b5%2bvbjv0ikmzrxseb621wgp6l%2f4aoakg%2fnkxcgah3t6omkqpxw5dxjw4v27zcxpcu52gkqefbrhhg%3d%3d&x-amz-algorithm=aws4-hmac-sha256&x-amz-date=20210124t022835z&x-amz-signedheaders=host&x-amz-expires=14400&x-amz-credential=asiawsmx3snwskq7z4xt%2f20210124%2fus-west-2%2fs3%2faws4_request&x-amz-signature=c683f06894332a60da509cf935ef198efa0308e35cbb7f99f9be498f9daeda32"
   
